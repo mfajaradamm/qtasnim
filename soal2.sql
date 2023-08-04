dd8d776c2816c9b49bc06b3c1a2e6f5d76ee3b37
mfajaradamm <139523080+mfajaradamm@users.noreply.github.com>
2023-08-04 09:44:02 +0700

    Create soal2.sql
    postgres - crud

---
+++ soal2.sql
@@ -0,0 +1,26 @@
+-- 1. Create
+CREATE DATABASE belajar;
+
+CREATE TABLE alatTulis (
+  id serial NOT NULL,
+  nama VARCHAR (225) NOT NULL,
+  harga VARCHAR (225) NOT NULL,
+  jumlah VARCHAR (225) NOT NULL,
+  PRIMARY KEY (id)
+)
+
+INSERT INTO pembeli (id,nama,harga,jumlah) VALUES 
+('1','pencil','Rp.3.000','1'),
+('2','pulpen','Rp.5.000','1'),
+('3','penghapus','Rp.2.000','1'),
+('4','penggaris','Rp.3.000','1'),
+
+-- 2. Read
+SELECT * FROM alattulis;
+SELECT nama FROM alattulis;
+
+-- 3. Update
+UPDATE alattulis SET harga = 'Rp.20.000' WHERE id = 4;
+
+-- 4. Delete
+DELETE FROM alattulis WHERE nama = 'penghapus';
